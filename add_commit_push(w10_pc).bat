0092ac61e9768f7cad816a9a64239772b0d51605
LJ <ljaouani29@gmail.com>
2024-01-18 12:28:51 +0100

    W10 PC 18.01.2024 12:28:50:29

--- add_commit_push(w10_pc).bat
+++ add_commit_push(w10_pc).bat
@@ -6,14 +6,7 @@ set commit_msg=W10 PC %datetime%
 
 git pull
 
-rem Füge nur Dateien hinzu, die keine JSON-Dateien sind
-for %%i in (*) do (
-    if /I "%%~xi" neq ".json" (
-        git add "%%i"
-    )
-)
-
-rem git add .
+git add .
 git commit -m "%commit_msg%"
 git push origin main
 
